f4adb93b974e9de1f0014ee7ba43cee7e85fd88c
unknown <adrianmercado782@gmail.com>
2026-02-12 08:22:01 -0400

    funcion multiplciar agregada

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,24 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicar";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
+const btnMultiplicar = document.querySelector("#btn-multiplicar");
 
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
+    event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+    const firstNumber = Number.parseInt(first.value);
+    const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+    div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+btnMultiplicar.addEventListener("click", () => {
+    const firstNumber = Number.parseInt(first.value);
+    const secondNumber = Number.parseInt(second.value);
+
+    div.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
 });
